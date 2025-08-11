370464145aa83d92f97512c1a9ca0b67fef255cd
kiran2396 <kirankumarca966@gmail.com>
2025-08-11 10:40:01 +0530

    first commit

---
+++ src/main/java/com/klu/Controller/ArithmeticCalc.java
@@ -0,0 +1,41 @@
+package com.klu.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+						
+@RestController
+@RequestMapping("/")
+@CrossOrigin("*")
+public class ArithmeticCalc {
+
+	@GetMapping("add/{A}/{B}")
+	public String add(@PathVariable ("A") int a, @PathVariable("B") int b) {
+		return "ADDITION = " + (a + b);
+	}
+	
+	@GetMapping("sub/{C}/{D}")
+	public String sub(@PathVariable ("C") int c, @PathVariable("D") int d) {
+		return "SUBTRACTION = " + (c - d );
+	}
+	
+	@GetMapping("multi/{A}/{B}")
+	public String multi(@PathVariable ("A") int a, @PathVariable("B") int b) {
+		return "MULTIPLICATION = " + (a * b);
+	}
+	
+	@GetMapping("div/{A}/{B}")
+	public String div(@PathVariable ("A") int a, @PathVariable("B") int b) {
+		return "DIVISION = " + (a / b);
+	}
+	
+	@GetMapping("mod/{A}/{B}")
+	public String mod(@PathVariable ("A") int a, @PathVariable("B") int b) {
+		return "MODULUS = " + (a % b);
+	}
+	
+}
