9aae867fb24cd81ef875a282e20cd1897c1dc620
Yaswanth-25052005 <yaswanthkotapati3@gmail.com>
2025-09-06 08:22:19 +0530

    second commit

--- src/main/java/com/klu/Controller/ArithmeticCalc.java
+++ src/main/java/com/klu/Controller/ArithmeticCalc.java
@@ -15,27 +15,27 @@ public class ArithmeticCalc {
 
 	@GetMapping("add/{A}/{B}")
 	public String add(@PathVariable ("A") int a, @PathVariable("B") int b) {
-		return "ADDITION = " + (a + b);
+		return "" + (a + b);
 	}
 	
 	@GetMapping("sub/{C}/{D}")
 	public String sub(@PathVariable ("C") int c, @PathVariable("D") int d) {
-		return "SUBTRACTION = " + (c - d );
+		return "" + (c - d );
 	}
 	
 	@GetMapping("multi/{A}/{B}")
 	public String multi(@PathVariable ("A") int a, @PathVariable("B") int b) {
-		return "MULTIPLICATION = " + (a * b);
+		return "" + (a * b);
 	}
 	
 	@GetMapping("div/{A}/{B}")
 	public String div(@PathVariable ("A") int a, @PathVariable("B") int b) {
-		return "DIVISION = " + (a / b);
+		return "" + (a / b);
 	}
 	
 	@GetMapping("mod/{A}/{B}")
 	public String mod(@PathVariable ("A") int a, @PathVariable("B") int b) {
-		return "MODULUS = " + (a % b);
+		return "" + (a % b);
 	}
 	
 }
